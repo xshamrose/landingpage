3b468717754847f07fbc6f7f0134758dbc00841e
SHAM ROSE <sham.rose.1815@gmail.com>
2025-05-16 19:41:09 +0530

    route changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme, CssBaseline, Box } from "@mui/material";
 import Header from "./components/Header";
 import BottomNav from "./components/BottomNav";
